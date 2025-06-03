f037c0f2df001601cd910eb68f3c87442e43416d
Tim Ung <timu@Tims-Mac-mini.local>
2025-06-03 16:32:14 +1000

    test csv paths and labels

--- src/testExtractCSVLabels.py
+++ src/testExtractCSVLabels.py
@@ -2,6 +2,9 @@ from dataset import extractCSVLabels
 from utils import UTKLabelType
 
 if __name__ == "__main__":
-    filepath = "../data/Balanced Datasets/balancedAge.csv"
-    csvPaths, csvLabels = extractCSVLabels(filepath, UTKLabelType.GENDER)
-    print(len(csvPaths), len(csvLabels))
+    labelType = UTKLabelType.GENDER
+    filepath = f"data/BalancedDatasets/balanced{labelType}.csv"
+    csvPaths, csvLabels = extractCSVLabels(filepath, labelType)
+    
+    print(csvLabels)
+    print(len(csvPaths), len(set(csvLabels)))
